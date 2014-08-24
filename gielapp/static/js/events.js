f217dd9b9d210431e27ea66f821e4d12e0e53081
Kirsten Ruys <kirsten.ruys@gmail.com>
2014-08-24 14:04:15 +0200

    animations

--- gielapp/static/js/events.js
+++ gielapp/static/js/events.js
@@ -8,40 +8,17 @@
 
 $(document).ready(function () {
 
-	//$('.tile-wrapper').on('click touch', openItem);
+	var overviewPos = {};
+
+	$('.tile-wrapper').on('click touch', openItem);
 	$('.playlist').on('click touch', showPlaylistAlert);
 	$('.icon-close, .icon-list').on('click touch', closeItem);
-
-	$('.tile-wrapper').click(function() {
-		  // $( ".overview-wrapper" ).animate({
-		  //   width: [ "toggle", "swing" ],
-		  //   height: [ "toggle", "swing" ],
-		  //   left: 50%,
-		  //   opacity: "toggle"
-		  // }, 5000, "swing", function() {
-		  //   console.log('yo done');
-		  // });
-		$(".overview-wrapper").css({position: "absolute", width: "90%"}).animate({
-        width: "toggle",
-        height: "toggle",
-        top: "50%",
-        left: "50%",
-        opacity: 0.4
-    }, {
-        duration: 500,
-        easing: "swing"
-    });
-
-});
-
-
-
 
 	/**
 	 * Opens the item of the clicked tile:
 	 * - gets the content of the tile (stored in global object tileContent)
 	 * - sets subTitle, itemTitle, and additional specific content
-	 * - hides the item overview and shows the item content
+	 * - animates the transition between item overview and item content
 	 * - triggers 'itemSet' to activate item specific eventHandlers
 	 */
 	function openItem(evt) {
@@ -50,28 +27,68 @@ $(document).ready(function () {
 		i = $('.tile-wrapper .tile').index(tile);
 		content = '';
 
-		if (tileContent[i].subTitle !== "") {
+		if (tileContent[i].subTitle !== '') {
 			content += '<h3>' + tileContent[i].subTitle + '</h3>';
 		}
 
-		if (tileContent[i].itemContent !== "") {
+		if (tileContent[i].itemContent !== '') {
 			content += tileContent[i].itemContent;
 		}
 
-		if (tileContent[i].itemTitle !== "") {
+		if (tileContent[i].itemTitle !== '') {
 			$('.item-wrapper h2 span.item-title').text(tileContent[i].itemTitle);
 		}
 		
 		$('.content').html(content);
 
-		$( ".overview-wrapper" ).fadeOut( "fast", function(){
-			$( ".item-wrapper" ).fadeIn( "fast" );
-		});
+		animateTransition();
 
 		$.event.trigger({
-			type: "itemSet",
+			type: 'itemSet',
 			pass: i
 		});
+	}
+
+	/**
+	 * Item overview is scaled down to its center until it's no longer visible,
+	 * Item rolls out from top.
+	 */
+	function animateTransition() {
+		var $overview, $item;
+		$overview = $('.overview-wrapper');
+		$item = $('.item-wrapper');
+
+		overviewPos = getAndSetPosition($overview);	//jQuery animate likes absolutely positioned elements!
+
+		$overview.animate({
+			width: ['toggle', 'swing'],
+			height: ['toggle', 'swing'],
+			left : '50%',
+			top : '50%',
+			opacity: 'toggle'
+		}, 400, 'swing', function() {
+			$item.animate({
+				height: ['toggle', 'swing'],
+				opacity: 'toggle'
+			}, 400, 'swing');
+		});
+	}
+
+	/**
+	 * gets the position of $el, sets $el to position absolute with the retrieved width, left, and top,
+	 * and returns the position. 
+	 */
+	function getAndSetPosition($el) {
+		var $el, pos;
+		$el = $el;
+		pos = {};
+
+		pos.w = $el.width();
+		pos.l = $el.position().left;
+		pos.t = $el.position().top;
+
+		$el.css({position: 'absolute', left:pos.l, top:pos.t, width:pos.w});
+		return pos;
 	}
 
 	/**
@@ -80,11 +97,28 @@ $(document).ready(function () {
 	 * - resets the margins between the tiles
 	 */
 	function closeItem() {
-		$( ".item-wrapper" ).fadeOut( "fast", function(){
-			$( ".overview-wrapper" ).fadeIn( "fast" );
-			setMargins();
+		var $overview = $('.overview-wrapper');
+
+		if ($overview.is(':visible')) {
+			return;
+		}
+
+		$('.item-wrapper').animate({
+				height: ['toggle', 'swing'],
+				opacity: 'toggle'
+			}, 400, 'swing', function() {
+				$overview.animate({
+					width: ['toggle', 'swing'],
+					height: ['toggle', 'swing'],
+					left : overviewPos.l,
+					top : overviewPos.t,
+					opacity: 'toggle'
+				}, 400, 'swing', function() {
+					$overview.css({position: 'relative', left: '', top: '', width: ''});
+					setMargins();
+			});
 		});
-		
+
 	}
 
 	/**
